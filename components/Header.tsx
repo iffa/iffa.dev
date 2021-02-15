c9d51904dc2e43078177cb6659deb7b504597a4c
Santeri Elo <me@santeri.xyz>
2021-02-15 23:07:14 +0200

    feat(ui): add github link to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,16 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import {
   Box,
+  ButtonGroup,
   Container,
   Flex,
   Heading,
   IconButton,
+  Link,
   useColorMode,
   useColorModeValue,
 } from '@chakra-ui/react';
+import { FaGithub } from 'react-icons/fa';
 import React from 'react';
 
 export default function Header(): JSX.Element {
@@ -17,16 +20,30 @@ export default function Header(): JSX.Element {
   return (
     <Box width="100%" py={4} backgroundColor={backgroundColor}>
       <Container maxWidth={'container.xl'}>
-        <Flex justifyContent="space-between" alignItems="center">
+        <Flex
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
           <Heading bgGradient="linear(to-l, #3F5EFB,#FC466B)" bgClip="text">
             iffa.dev
           </Heading>
-          <IconButton
-            aria-label="Toggle color mode"
-            title="Toggle color mode"
-            icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-            onClick={toggleColorMode}
-          />
+
+          <ButtonGroup size="md">
+            <Link href={process.env.NEXT_PUBLIC_GITHUB_URL} isExternal>
+              <IconButton
+                aria-label="Navigate to GitHub"
+                title="Navigate to GitHub"
+                icon={<FaGithub />}
+              />
+            </Link>
+            <IconButton
+              aria-label="Toggle color mode"
+              title="Toggle color mode"
+              icon={colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              onClick={toggleColorMode}
+            />
+          </ButtonGroup>
         </Flex>
       </Container>
     </Box>
