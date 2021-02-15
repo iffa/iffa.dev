268eb247c86312c8e52083cfefd33e1fbe02b46e
Santeri Elo <me@santeri.xyz>
2021-02-15 22:12:29 +0200

    feat(ui): explicit colormode globals

--- styles/app.theme.ts
+++ styles/app.theme.ts
@@ -1,8 +1,24 @@
 import { extendTheme } from '@chakra-ui/react';
 
+function mode(lightMode, darkMode) {
+  return (props) => (props.colorMode === 'dark' ? darkMode : lightMode);
+}
+
 export const AppTheme = extendTheme({
   fonts: {
-    heading: 'Inconsolata',
-    body: 'Montserrat',
+    heading: 'Inconsolata, monospace',
+    body: 'Montserrat, sans-serif',
+    mono: 'Inconsolata, monospace',
   },
+  global: (props) => ({
+    color: mode('gray.800', 'whiteAlpha.900')(props),
+    bg: mode('white', 'gray.800')(props),
+    '*::placeholder': {
+      color: mode('gray.400', 'whiteAlpha.400')(props),
+    },
+    '*, *::before, &::after': {
+      borderColor: mode('gray.200', 'whiteAlpha.300')(props),
+      wordWrap: 'break-word',
+    },
+  }),
 });
