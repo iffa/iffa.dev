3fd30c78ab7dae6f6f25696d66912958433f1658
Santeri Elo <me@santeri.xyz>
2021-02-22 23:12:46 +0200

    feat(ui): add dev icon particles to header bg

--- pages/layout-test.tsx
+++ pages/layout-test.tsx
@@ -3,11 +3,100 @@ import {
   Container,
   Flex,
   Heading,
-  Image,
   Text,
   useBreakpointValue,
 } from '@chakra-ui/react';
 import React from 'react';
+import Particles from 'react-tsparticles';
+import { ISourceOptions } from 'tsparticles';
+
+const particleConfig: ISourceOptions = {
+  fpsLimit: 30,
+  pauseOnBlur: true,
+  pauseOnOutsideViewport: true,
+  detectRetina: true,
+  backgroundMode: {
+    enable: true,
+  },
+  motion: {
+    disable: true,
+  },
+  particles: {
+    reduceDuplicates: true,
+    collisions: {
+      enable: true,
+    },
+    links: {
+      enable: true,
+      consent: true,
+      warp: true,
+      color: {
+        value: 'random',
+      },
+      opacity: 0.75,
+    },
+    move: {
+      enable: true,
+      direction: 'none',
+      outMode: 'bounce',
+      random: true,
+      speed: 1,
+    },
+    number: {
+      limit: 100,
+      value: 100,
+    },
+    opacity: {
+      value: 0.5,
+      random: {
+        enable: true,
+        minimumValue: 0.1,
+      },
+    },
+    size: {
+      value: 16,
+      random: {
+        enable: true,
+        minimumValue: 8,
+      },
+    },
+    shape: {
+      type: 'images',
+      images: [
+        {
+          src: '/icons/angular.png',
+          width: 128,
+          height: 128,
+        },
+        {
+          src: '/icons/android.png',
+          width: 128,
+          height: 128,
+        },
+        {
+          src: '/icons/javascript.png',
+          width: 128,
+          height: 128,
+        },
+        {
+          src: '/icons/react.png',
+          width: 128,
+          height: 128,
+        },
+        {
+          src: '/icons/kotlin.png',
+          width: 128,
+          height: 128,
+        },
+        {
+          src: '/icons/node.png',
+          width: 128,
+          height: 128,
+        },
+      ],
+    },
+  },
+};
 
 export default function LayoutTest(): JSX.Element {
   const headingSize = useBreakpointValue({ base: 'header', lg: 'header-xl' });
@@ -35,12 +124,14 @@ export default function LayoutTest(): JSX.Element {
                 writingMode: 'vertical-rl',
                 textOrientation: 'upright',
               }}
+              zIndex="overlay"
               isTruncated
             >
               santeri
             </Heading>
           </Flex>
         </Container>
+        <Particles options={particleConfig} />
       </Box>
       <Box as="main" flexBasis={{ base: 'auto', lg: '50%' }}>
         {numbers.map((x, index) => {
